6f9b938e0801156c293118c37cfc056015c9c42d
agaharwar1 <44177381+agaharwar1@users.noreply.github.com>
2018-10-27 21:46:36 +0530

    Update pyramidstar.java

--- pyramidstar.java
+++ pyramidstar.java
@@ -1,18 +1,22 @@
 class pyramidstar {
 
-    public static void main(String[] args) {
+public static void main(String args[]) {
 
-        for(int a=5;a>0;a--){
-            int b=0;
-            for(b=0;b<a;b++){
+        for(int a=1;a<=8;a++) {
+            for (int b = 8; b >= a; b--) {
                 System.out.print(" ");
-                
-            }
-            for (int j=b;j<5;j++){
-                System.out.print(" * ");
-            }
-            System.out.println("");
 
-        }
-    }
+            }
+            for (int j = 1; j < (a * 2); j++) {
+                if(j>1 && j<(a*2)-1)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("*");
+
+            }}
+            System.out.println();
+        }    }
 }
