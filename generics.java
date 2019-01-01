70040deb4ae60fa40d7187fcab02520cc2ab1d60
agaharwar1 <44177381+agaharwar1@users.noreply.github.com>
2019-01-01 08:09:54 +0530

    Add files via upload

---
+++ generics.java
@@ -0,0 +1,16 @@
+package generics;
+import java.util.ArrayList;
+
+public class generics {
+	public static void main(String[] args) {
+			ArrayList<String> strings = new ArrayList<String>();
+			strings.add("cat");
+			strings.add("dog");
+			System.out.println(strings);
+			
+					}
+
+	}
+
+
+
